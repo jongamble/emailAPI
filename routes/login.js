cfc8c3db4c2dab883105f27dbbf5de4475a76503
Jon Gamble <jon@royalflushdesigns.net>
2014-02-28 15:28:53 -0500

    Version 2 Base Tutorial
    setting up the base for version 2

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-exports.form = function(req, res) {
-		res.render('login', { title: 'Login', copy: 'Please enter your log in credentials below.' });
-};
-
-exports.authenticate = function(passport) {
-	return function(req, res) {
-		
-	}
-};
