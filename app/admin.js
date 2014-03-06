d7f1d128b1192dbc398422404b43f4cd812388b3
Jon Gamble <jon@royalflushdesigns.net>
2014-03-06 14:10:53 -0500

    Adding Database information for admin panel

---
+++ app/admin.js
@@ -0,0 +1,24 @@
+// app/admin.js
+module.exports = function(app, db) {
+
+	// =====================================
+	// PROFILE SECTION =====================
+	// =====================================
+	// we will want this protected so you have to be logged in to visit
+	// we will use route middleware to verify this (the isLoggedIn function)
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile.ejs', {
+			user : req.user // get the user out of session and pass to template
+		});
+	});
+
+	app.get('/admin', isLoggedIn, isAdmin,function(req, res, db) {
+		db.collection('userlist').find().toArray(function (err, items) {
+			res.render('admin.ejs', {
+				items : userList // get the user out of session and pass to template
+			});
+	   	});
+	});
+
+
+};
--- app/routes.js
+++ app/routes.js
@@ -42,28 +42,7 @@ module.exports = function(app, passport) {
 		failureFlash : true // allow flash messages
 	}));
 
-	// process the signup form
-	// app.post('/signup', do all our passport stuff here);
-
-	// =====================================
-	// PROFILE SECTION =====================
-	// =====================================
-	// we will want this protected so you have to be logged in to visit
-	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
-			user : req.user // get the user out of session and pass to template
-		});
-	});
-
-	app.get('/admin', isLoggedIn, isAdmin,function(req, res) {
-		db.collection('userlist').find().toArray(function (err, items) {
-			res.render('admin.ejs', {
-				items : userList // get the user out of session and pass to template
-			});
-	   	});
-	});
-
+	
 	// =====================================
 	// LOGOUT ==============================
 	// =====================================
