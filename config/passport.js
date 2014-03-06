455f5315eb0d52cbab783e012fa9b91c8cf3fa61
Jon Gamble <jon@royalflushdesigns.net>
2014-03-06 12:26:21 -0500

    Login Testing

--- config/passport.js
+++ config/passport.js
@@ -92,15 +92,15 @@ module.exports = function(passport) {
 
     passport.use('local-login', new LocalStrategy({
         // by default, local strategy uses username and password, we will override with email
-        usernameField : 'email',
+        usernameField : 'domain',
         passwordField : 'password',
         passReqToCallback : true // allows us to pass back the entire request to the callback
     },
-    function(req, email, password, done) { // callback with email and password from our form
+    function(req, domain, password, done) { // callback with email and password from our form
 
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        User.findOne({ 'local.email' :  email }, function(err, user) {
+        User.findOne({ 'local.domain' :  domain }, function(err, user) {
             // if there are any errors, return the error before anything else
             if (err)
                 return done(err);
