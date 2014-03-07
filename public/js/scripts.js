37b2ebb10d24c2ce7ff643a2fc6d5c27a3bd5c41
Jon Gamble <jon@royalflushdesigns.net>
2014-03-07 09:29:50 -0500

    transition ajax to ng for delete user

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,25 +1,5 @@
-// Delete User
-function deleteUser(event) {
-  event.preventDefault();
-  var confirmation = confirm('Are you sure you want to delete this user?');
-  if (confirmation === true) {
-    $.ajax({
-      type: 'DELETE',
-      url: '/deleteuser/' + $(this).attr('rel')
-    }).done(function( response ) {
-      if (response.msg === '') {}
-      else {
-        alert(response.msg);
-      }
-      window.reload();
-    });
-  }
-  else {
-    return false;
-  }
-};
 
 
 jQuery(document).ready(function($) {
-  $('.deleteLink').on('click', deleteUser);
+  
 });
