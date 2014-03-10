de177c06de800c20c79ef81c07b07668cbe1b1de
Jon Gamble <jon@royalflushdesigns.net>
2014-03-10 15:20:26 -0400

    Adding Ajax call for listing profile leads

--- public/js/core.js
+++ public/js/core.js
@@ -39,4 +39,47 @@ function mainController($scope, $http) {
 			});
 	};
 
+};
+
+
+var profileFunctions = angular.module('profileFunctions', []);
+
+function mainController($scope, $http) {
+	$scope.formData = {};
+
+	// when landing on the page, get all todos and show them
+	$http.get('/profile/listLeads')
+		.success(function(data) {
+			$scope.users = data;
+			//console.log(data);
+		})
+		.error(function(data) {
+			console.log('Error: ' + data);
+		});
+
+	// when submitting the add form, send the text to the node API
+	// $scope.createTodo = function() {
+	// 	$http.post('/api/todos', $scope.formData)
+	// 		.success(function(data) {
+	// 			$scope.formData = {}; // clear the form so our user is ready to enter another
+	// 			$scope.todos = data;
+	// 			console.log(data);
+	// 		})
+	// 		.error(function(data) {
+	// 			console.log('Error: ' + data);
+	// 		});
+	// };
+
+	// delete a todo after checking it
+	$scope.deleteLead = function(id) {
+		$http.delete('/profile/deleteLead/' + id)
+			.success(function(data) {
+				$scope.users = data;
+				//console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
 }
