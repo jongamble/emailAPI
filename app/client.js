f062856e600618a3d578a4f68afe71f48d7cbd35
Jon Gamble <jon@royalflushdesigns.net>
2014-03-10 15:10:35 -0400

    Folder structure

--- app/client.js
+++ app/client.js
@@ -10,7 +10,7 @@ module.exports = function(app, mongoose, passport) {
 	// we will want this protected so you have to be logged in to visit
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/profile', isLoggedIn, function(req, res) {
-		res.render('profile.ejs', {
+		res.render('profile/profile.ejs', {
 			user : req.user // get the user out of session and pass to template
 		});
 	});
