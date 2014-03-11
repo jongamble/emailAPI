238aaf359fb8e2fd895aba99a68d557ea3740131
Jon Gamble <jon@royalflushdesigns.net>
2014-03-11 15:31:10 -0400

    Adding api check logic

--- app/api.js
+++ app/api.js
@@ -6,14 +6,16 @@ module.exports = function(app, mongoose, passport) {
 
 	// process the edit user form
 	app.post('/api/newLead/:id', function(req, res) {
-		var lead = new Lead();
-			
-		lead.clientID = req.params.id;
-		lead.content = req.body;
+		if(User.find({_id: req.params.id})){
+			var lead = new Lead();
+				
+			lead.clientID = req.params.id;
+			lead.content = req.body;
 
-		lead.save(function(err){
-			if (err) console.log(err);
-			res.redirect('/profile');
-		});
+			lead.save(function(err){
+				if (err) console.log(err);
+				res.redirect('/profile');
+			});
+		}
 	});
 };
